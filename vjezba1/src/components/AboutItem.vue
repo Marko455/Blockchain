51e0c3df22284e5c0653ad87dade126007079f32
Marko Galavić <147241279+Marko455@users.noreply.github.com>
2025-10-24 11:17:56 +0200

    Prva vjezba

---
+++ vjezba1/src/components/AboutItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="item">
+        <h1>{{ title }}</h1>
+        <h1>Broj: {{ broj }}</h1>
+        <button @click="povecajBroj">Povacaj broj</button>
+        <button @click="smanjiBroj">Smanji broj</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+    const broj = ref(0);
+    const props = defineProps({title: String})
+    const emit = defineEmits(['Inkrementirano', 'Dekrementirano'])
+
+    function povecajBroj(){
+        broj.value++;
+        emit('Inkrementirano')
+        console.log("Inkrementirano")
+    }
+    function smanjiBroj(){
+        broj.value--;
+        emit('Dekrementirano')
+        console.log("Dekrementirano")
+    }
+</script>
--- vjezba1/src/views/AboutView.vue
+++ vjezba1/src/views/AboutView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AboutItem title> O nama</AboutItem>
   </div>
 </template>
+
+<script setup>
+  import AboutItem from '@/components/AboutItem.vue';
+</script>
 
 <style>
 @media (min-width: 1024px) {
